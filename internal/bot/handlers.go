85f8b21e42f30fba5295f371344cb02eca2da034
Max Reiter <max.m.reiter@gmail.com>
2023-07-20 15:34:11 -0700

    handle GUILD_CREATE

--- internal/bot/handlers.go
+++ internal/bot/handlers.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	"github.com/diamondburned/arikawa/v3/gateway"
+	"github.com/holedaemon/bot2/internal/db/models"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"github.com/zikaeroh/ctxlog"
 	"go.uber.org/zap"
 )
@@ -14,6 +17,32 @@ func (b *Bot) onReady(r *gateway.ReadyEvent) {
 
 func (b *Bot) onReconnect(r *gateway.ReconnectEvent) {
 	b.Logger.Info("reconnected to Discord gateway")
+}
+
+func (b *Bot) onGuildCreate(g *gateway.GuildCreateEvent) {
+	ctx := context.Background()
+	log := b.Logger.With(zap.String("guild_id", g.ID.String()))
+
+	exists, err := models.Guilds(qm.Where("guild_id = ?", g.ID.String())).Exists(ctx, b.DB)
+	if err != nil {
+		log.Error("error querying guild", zap.Error(err))
+		return
+	}
+
+	if exists {
+		return
+	}
+
+	guild := &models.Guild{
+		GuildID: g.ID.String(),
+	}
+
+	if err := guild.Insert(ctx, b.DB, boil.Infer()); err != nil {
+		log.Error("error inserting guild into database", zap.Error(err))
+		return
+	}
+
+	log.Info("created record for guild")
 }
 
 func (b *Bot) onMessage(m *gateway.MessageCreateEvent) {
