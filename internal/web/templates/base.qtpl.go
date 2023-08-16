59f240730d55ec912d460e57a4ba85368bcec00b
Max Reiter <max.m.reiter@gmail.com>
2023-08-16 13:16:09 -0700

    navbar tweaking

--- internal/web/templates/base.qtpl.go
+++ internal/web/templates/base.qtpl.go
@@ -158,7 +158,7 @@ func (p *BasePage) StreamNavbar(qw422016 *qt422016.Writer) {
 <nav class="bg-white border-gray-200 dark:bg-stone-950">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DILF</span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white dark:hover:text-green-500">DILF</span>
     </a>
     <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-stone-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
@@ -167,7 +167,7 @@ func (p *BasePage) StreamNavbar(qw422016 *qt422016.Writer) {
         </svg>
     </button>
     <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-      <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-stone-900 md:dark:bg-gray-950">
+      <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-stone-900 md:dark:bg-stone-950">
         <li>
           <a href="/about" class="block py-2 pl-3 pr-4 text-white rounded dark:hover:bg-green-500 md:p-0 md:dark:hover:text-green-500 md:dark:bg-transparent md:dark:hover:bg-transparent">About</a>
         </li>
